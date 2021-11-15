03bd6392e0d2edd9c3591ffe262caa78634783d7
Jorge A. Ayala <Darkkhir0@gmail.com>
2021-11-14 22:02:48 -0500

    Se agrega actividad de formulario de registro

--- app/src/main/java/com/misiontic/proyecto/MainActivity.kt
+++ app/src/main/java/com/misiontic/proyecto/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.misiontic.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnRegistrar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRegistrar = findViewById(R.id.button2)
+
+        btnRegistrar.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
